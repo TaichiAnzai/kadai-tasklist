e34da47ca7989c632c822c61ec6e19d183412285
TaichiAnzai <taichi.anzai.7a@gmail.com>
2017-04-25 00:34:54 +0000

    Add log in

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy, :update, :edit, :show]
   
   def index
     @tasks = Task.order(created_at: :desc).page(params[:page])
@@ -14,14 +15,16 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました。'
       redirect_to @task
+      #redirect_to root_url
     else
       flash.now[:danger] = 'タスクが投稿されませんでした。'
       render :new
+      #render 'toppages/index'
     end
   end
 
@@ -46,7 +49,9 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    #redirect_to tasks_url
+    redirect_to root_url
+    #redirect_back(fallback_location: root_path)
   end
   
   private
@@ -57,6 +62,14 @@ class TasksController < ApplicationController
    # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+      #redirect_to @task
+    end
   end
   
 end
